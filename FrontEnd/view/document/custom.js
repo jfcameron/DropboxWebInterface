ad7741ed2cb826b2a71603bf29146d3c42e59277
Joseph Cameron <jf.cameron@gmail.com>
2017-02-28 22:01:45 -0500

    improved document and image views
    document now supplies a download link at the top of the viewport.
    
    image now scales more approprately to fit the image in viewport

--- FrontEnd/view/document/custom.js
+++ FrontEnd/view/document/custom.js
@@ -1,3 +1,4 @@
 var src =($_GET("f")? $_GET("f"): "/");
 
-document.getElementById('document-view').src = "https://docs.google.com/gview?url="+src+"&embedded=true";   
+document.getElementById('downloadlink').innerHTML = "<a href=\'"+src+"\'>"+src.split("/").pop()+"</a>";
+document.getElementById('document-view').src = "https://docs.google.com/gview?url="+src+"&embedded=true";
